a3f82681625fe17b90639bad3dd0d6dba0a6f285
m4rrc0 <github-h9s2@m4rr.co>
2025-03-16 15:07:15 +0100

    adapter depends on env vars found

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,18 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
-// import cloudflare from '@astrojs/cloudflare';
+import cloudflare from '@astrojs/cloudflare';
 import netlify from '@astrojs/netlify';
+
+const isBuildingOnNetlify = Boolean(process.env.NETLIFY);
+const isBuildingOnCloudflare = Boolean(process.env.CF_PAGES);
+
+const adapter = isBuildingOnNetlify ? netlify() : (isBuildingOnCloudflare ? cloudflare() : undefined);
 
 // https://astro.build/config
 export default defineConfig({
   srcDir: './cms',
   integrations: [react(), markdoc(), keystatic()],
   // adapter: cloudflare(),
-  adapter: netlify(),
+  adapter,
 });
